05c6328c890e4af79ecc534a25ba2f11a19cd41d
Ted M. Young <tedyoung@gmail.com>
2022-02-22 16:08:38 -0800

    LAB #4: Test for Player Hits and Goes Bust passes

---
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -0,0 +1,23 @@
+package com.jitterted.ebp.blackjack.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GameOutcomeTest {
+
+    @Test
+    public void playerHitsAndGoesBustThenOutcomeReturnsPlayerBusted() throws Exception {
+        Deck playerHitsAndGoesBustDeck = new StubDeck(Rank.TEN, Rank.EIGHT,
+                                                      Rank.QUEEN, Rank.JACK,
+                                                      Rank.THREE);
+        Game game = new Game(playerHitsAndGoesBustDeck);
+        game.initialDeal();
+
+        game.playerHits();
+
+        assertThat(game.determineOutcome())
+                .isEqualTo("You Busted, so you lose.  💸");
+    }
+
+}
