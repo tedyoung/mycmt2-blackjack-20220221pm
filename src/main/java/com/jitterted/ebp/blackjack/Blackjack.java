0a894470a12a96c9a391431dd8b2a6e1a4eef1e2
Ted M. Young <tedyoung@gmail.com>
2022-02-22 16:02:35 -0800

    LAB #4: Prepare refactoring: have callers to Game constructor provide the Deck to use

--- src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
+++ src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
@@ -1,6 +1,7 @@
 package com.jitterted.ebp.blackjack;
 
 import com.jitterted.ebp.blackjack.adapter.in.console.ConsoleGame;
+import com.jitterted.ebp.blackjack.domain.Deck;
 import com.jitterted.ebp.blackjack.domain.Game;
 
 // Startup-Assembler class
@@ -9,7 +10,7 @@ public class Blackjack {
     // Creates, Assembles (wires), and Configures objects together
     // This class is Transient
     public static void main(String[] args) {
-        Game game = new Game(); // Entity-like object
+        Game game = new Game(new Deck()); // Entity-like object
         ConsoleGame consoleGame = new ConsoleGame(game); // in general: Entities aren't directly passed in to Adapters
         consoleGame.start();
     }
