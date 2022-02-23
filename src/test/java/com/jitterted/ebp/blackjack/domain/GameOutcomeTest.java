fad3af6ef4e55e9371bf5d04a25763022dfb2ee8
Ted M. Young <tedyoung@gmail.com>
2022-02-22 16:10:50 -0800

    LAB #4: Test for Player Stands and Beats Dealer passes

--- src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -20,4 +20,18 @@ class GameOutcomeTest {
                 .isEqualTo("You Busted, so you lose.  💸");
     }
 
+    @Test
+    public void playerDealtBetterHandThanDealerAndStandsThenPlayerBeatsDealer() throws Exception {
+        Deck playerStandsAndBeatsDealer = new StubDeck(Rank.TEN,   Rank.EIGHT,
+                                                       Rank.QUEEN, Rank.JACK);
+        Game game = new Game(playerStandsAndBeatsDealer);
+        game.initialDeal();
+
+        game.playerStands();
+        game.dealerTurn();
+
+        assertThat(game.determineOutcome())
+                .isEqualTo("You beat the Dealer! 💵");
+    }
+
 }
