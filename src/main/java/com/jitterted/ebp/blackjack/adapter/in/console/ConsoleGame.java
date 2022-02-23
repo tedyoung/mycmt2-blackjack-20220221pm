88284c354aa8d6f17ce126c143f8ed898a88fa93
Ted M. Young <tedyoung@gmail.com>
2022-02-22 16:21:27 -0800

    LAB #4: Extracted GameOutcome enum and updated tests and usage

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
@@ -31,7 +31,7 @@ public class ConsoleGame {
 
         displayFinalGameState();
 
-        System.out.println(game.determineOutcome());
+        System.out.println(game.determineOutcome().display());
 
         resetScreen();
     }
