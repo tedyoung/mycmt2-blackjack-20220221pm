6ee126b56a849727cabbb3904070dc416a6ade07
Ted M. Young <tedyoung@gmail.com>
2022-03-08 15:59:02 -0800

    LAB #8C: Implemented concrete HTTP Game Monitor adapter and wired it to the application.

---
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/out/gamemonitor/HttpGameMonitor.java
@@ -0,0 +1,27 @@
+package com.jitterted.ebp.blackjack.adapter.out.gamemonitor;
+
+import com.jitterted.ebp.blackjack.domain.Game;
+import com.jitterted.ebp.blackjack.domain.port.GameMonitor;
+import org.springframework.web.client.RestTemplate;
+
+public class HttpGameMonitor implements GameMonitor {
+
+    @Override
+    public void roundCompleted(Game game) {
+        // transform GAME -> DTO
+        GameResultDto gameResultDto = GameResultDto.of(game);
+        // send the DTO to the remote service
+        try {
+            post("https://blackjack-game-monitor.herokuapp.com/api/gameresults",
+                 gameResultDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void post(String uri, GameResultDto gameResultDto) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForObject(uri, gameResultDto, GameResultDto.class);
+    }
+
+}
