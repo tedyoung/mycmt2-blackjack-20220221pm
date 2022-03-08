be459266a3101349647826ccc94f2cb7d2881cd7
Ted M. Young <tedyoung@gmail.com>
2022-03-08 15:26:16 -0800

    LAB #8A&B: First Scenario: Player Stands, results reported to Game Monitor

---
+++ src/test/java/com/jitterted/ebp/blackjack/domain/port/GameMonitorTest.java
@@ -0,0 +1,28 @@
+package com.jitterted.ebp.blackjack.domain.port;
+
+// use these static imports in the test class
+
+import com.jitterted.ebp.blackjack.domain.Game;
+import com.jitterted.ebp.blackjack.domain.StubDeck;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+class GameMonitorTest {
+
+    @Test
+    public void playerStandsThenGameIsOverAndResultsSentToMonitor() throws Exception {
+        GameMonitor gameMonitorSpy = spy(GameMonitor.class);
+        Game game = new Game(StubDeck.playerStandsAndBeatsDealer(), gameMonitorSpy);
+        game.initialDeal();
+
+        game.playerStands();
+
+        // verify that the roundCompleted method was called with any instance of a Game class
+        verify(gameMonitorSpy).roundCompleted(any(Game.class));
+    }
+
+
+}
